098265053e829a2ab4762d3d7960efd7cfd431b0
khiemvn24 <91418194+khiemvn24@users.noreply.github.com>
2021-11-03 14:20:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -121,23 +121,6 @@ function bai26(){
 }
 // Cho một mảng là một tập hợp các học viên trong lớp REACTJS, mảng là một tập hợp object của học viên gồm 3 thông tin: id, firstName, lastName, và age. 
 // Hãy đưa tên và họ về chuẩn tên với ký tự đầu tiên của tên Viết Hoa và tìm tất cả học viên có tên tồn tại chữ cái “a” hoặc “A” và tên dài hơn hoặc bằng 3 ký tự.
-
-let students = [{
-        id: "T3HXX1",
-        firstName: "NgAN",
-        lastName: "Duong Thuy"
-    },
-    {
-        id: "T3HXX2",
-        firstName: "Ha",
-        lastName: "Do Thi Thu"
-    },
-    {
-        id: "T3HXX5",
-        firstName: "Minh",
-        lastName: "Nguyen Nhat"
-    }
-]
 function chuanHoa(chuoi){
     chuoi = chuoi.toLowerCase();
     chuoi = chuoi.split(" ");
